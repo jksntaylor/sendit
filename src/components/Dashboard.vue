d79a3d2a3e55ddc9bac5d2bfe9cc5cd1b8538f06
jksntaylor <josephjacksontaylor@gmail.com>
2020-01-09 17:36:53 -0700

    input validation

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -6,14 +6,16 @@
         <Location/>
         <Weather/>
         <Webcams/>
-        <input v-model="name" class="name"/>
-        <select v-model="resort " class="resort">
+        <input v-model="name" class="name" v-bind:class="{ missing: noName }"/>
+        <select v-model="resort " class="resort" v-bind:class="{ missing: noResort }">
+            <option value=""/>
             <option value="Alta">Alta</option>
             <option value="Snowbird">Snowbird</option>
             <option value="Brighton">Brighton</option>
             <option value="Solitude">Solitude</option>
         </select>
         <button @click="submit" class="submit">SEND IT</button>
+        <button @click="clearCookie" class="clearCookie">Clear Cookie</button>
     </div>
 </template>
 
@@ -27,16 +29,17 @@ export default {
     name: 'Dashboard',
     data() {
         return {
-            name: this.$cookie.get('name'),
-            resort: '',
+            name: this.$cookie.get('name') || '',
+            resort: this.$cookie.get('resort') || '',
             time: new Date().toLocaleTimeString([], {hour: "numeric", minute: "2-digit"}),
-            date: new Date().toLocaleDateString([], {weekday: "long", month: "long", day: "numeric", year:"numeric"})
+            date: new Date().toLocaleDateString([], {weekday: "long", month: "long", day: "numeric", year:"numeric"}),
+            noResort: false,
+            noName: false
         }
     },
     mounted: function () {
-        $data.name = this.$cookie.get('name');
-        $data.resort = this.$cookie.get('resort');
-        console.log(name, resort);
+        this.$data.name = this.$cookie.get('name');
+        this.$data.resort = this.$cookie.get('resort');
     },
     components: {
         Lifts,
@@ -46,13 +49,24 @@ export default {
     },
     methods: {
         submit() {
-            this.$cookie.set('name', this.$data.name, {expires: '5Y'});
-            this.$cookie.set('resort', this.$data.resort, {expires: '5Y'});
+            this.$data.noResort = this.$data.resort ? false : true;
+            this.$data.noName = this.$data.name ? false : true;
+
+            if (this.$data.name&&this.$data.resort) {
+                this.$cookie.set('name', this.$data.name, {expires: '5Y'});
+                this.$cookie.set('resort', this.$data.resort, {expires: '5Y'});
+            }
+        },
+        clearCookie() {
+            this.$cookie.delete('name');
+            this.$cookie.delete('resort');
         }
     }
 }
 </script>
 
 <style scoped lang="scss">
-
+    .missing {
+        border: 2px solid red;
+    }
 </style>
