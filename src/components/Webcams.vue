992b5b68c44653f26fc511676e0cdded6f8275c5
jksntaylor <josephjacksontaylor@gmail.com>
2020-01-12 13:50:22 -0700

    webcam data coming through. styling is weird, but can fix that later

--- src/components/Webcams.vue
+++ src/components/Webcams.vue
@@ -1,6 +1,22 @@
 <template>
     <div class="cams">
-        
+        <b-carousel
+        id="carousel-1"
+        v-model="slide"
+        :interval="8000"
+        indicators
+        img-height="224px"
+        img-width="400px"
+        background="#ababab"
+        @sliding-start="onSlideStart"
+        @sliding-end="onSlideEnd">
+            <b-carousel-slide v-bind:key="index" v-for="(item, index) in cams">
+                <h1 class="name">{{item.name}}</h1>
+                <a v-bind:href="item.source" class="webcam-link" target="_blank" rel="noopener noreferrer">
+                    <img v-bind:src="item.image" v-bind:alt="item.name" class="webcam-still">
+                </a>
+            </b-carousel-slide>
+        </b-carousel>
     </div>
 </template>
 
@@ -9,12 +25,25 @@ export default {
     name: 'Webcams',
     data() {
         return {
-            greeting: 'HelloWebcams'
+            cams: this.$store.state.resortInfo.webcams,
+            slide: 0,
+            sliding: null
         }
+    },
+    methods: {
+        onSlideStart(slide) {
+        this.sliding = true
+      },
+      onSlideEnd(slide) {
+        this.sliding = false
+      }
     }
 }
 </script>
 
 <style scoped lang="scss">
-
+    .webcam-still {
+        height: 112px;
+        width: 200px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import VueCookie from 'vue-cookie';
+import { BootstrapVue } from 'bootstrap-vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import store from './store'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 Vue.use(VueCookie);
+Vue.use( BootstrapVue )
 
 new Vue({
   store,
